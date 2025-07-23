5b9ff0a6947bd599746b237a5b8ed1d2c55c3a1a
Guillaume Slizewicz <guillaume.slizewicz@hotmail.fr>
2025-07-23 18:23:29 +0200

    changed mobile layout

--- server.js
+++ server.js
@@ -248,8 +248,8 @@ app.get('/', (req, res) => {
           }
           
           @media (max-width: 600px) {
-            body { padding: 5px 5px; font-size: 20px; }
-            .ascii-art { font-size: 20px; }
+            body { padding: 5px 5px; font-size: 18px; }
+            .ascii-art { font-size: 18px; }
           }
         /* layered ASCII animation */
         .ascii-stack      { position: relative; display: inline-block; }
